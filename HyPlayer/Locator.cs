c49d208ee4c1f40d64eb6ab34509eead5a350b1b
Jeff Zeng <zhh13950510580@outlook.com>
2025-01-26 18:20:12 +0800

    [Feat] 添加 Locator。

---
+++ HyPlayer/Locator.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+using System;
+
+
+
+namespace Flarum;
+
+public class Locator
+{
+    public static Locator Instance => _Instance ?? (_Instance = new Locator());
+    private static Locator _Instance;
+
+    private IServiceProvider _services;
+
+    public T GetService<T>()
+        where T : class
+    {
+        if (_services.GetService(typeof(T)) is not T service)
+        {
+            throw new Exception($"{typeof(T)} needs to be regiestered in ConfigureServices.");
+        }
+
+        return service;
+    }
+
+    public Locator()
+    {
+        var _servicesCollection = new ServiceCollection();
+
+        _services = _servicesCollection.BuildServiceProvider();
+
+    }
+
+}
+
+
